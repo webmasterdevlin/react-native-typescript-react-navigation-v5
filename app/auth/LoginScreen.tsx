5ec432eb7a54bb95b62d2ee5e945c84f4b3d1c50
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-01-12 10:37:33 +0100

    updated

--- app/auth/LoginScreen.tsx
+++ app/auth/LoginScreen.tsx
@@ -2,14 +2,55 @@ import React from 'react';
 import { View, Text } from 'react-native'
 import { Button } from 'react-native-paper';
 
-const LoginScreen = () => {
-    return (<View>
-        <Text>Hello LoginScreen</Text>
-        <Text>Hello LoginScreen</Text>
-        <Text>Hello LoginScreen</Text>
-        <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
-            Press me
+class LoginScreen extends React.Component {
+    state = {
+        firstName: 'old default value'
+    }
+
+    myFunction = () => {
+        this.setState({ firstName: "the new value" })
+    }
+
+    componentDidMount() {
+        // HTTP Requests
+    }
+
+    render() {
+        return (<View>
+            <Text>Hello LoginScreen</Text>
+            <Text>Hello LoginScreen</Text>
+            <NewText newerName="Crayon" clickButton={this.myFunction} />
+            <Button icon="camera" mode="contained" onPress={() => this.myFunction()}>
+                Press me
         </Button>
-    </View>)
+        </View>)
+    }
 }
 export default LoginScreen;
+
+interface IProps {
+    newerName: string | number | boolean,
+    newAge?: number,
+    clickButton: () => void
+}
+
+type Props = {
+    newerName: string | number | boolean,
+    newAge?: number,
+    clickButton: () => void
+}
+
+const NewText: React.FC<IProps> = (anyObjName) => {
+    const [companyName, setCompanyName] = React.useState('Crayon');
+    const [anotherState, setAnotherState] = React.useState(0);
+
+    React.useEffect(() => {
+        setCompanyName(anyObjName.newerName)
+    }, [])
+
+    return (<View>
+        <Button onPress={() => anyObjName.clickButton}>Click</Button>
+        <Text style={{ fontSize: 50 }}>{companyName}</Text>
+    </View>)
+}
+
