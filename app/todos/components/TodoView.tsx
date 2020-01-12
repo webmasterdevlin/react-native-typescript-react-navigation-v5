dcd2d92bdb207600df2dff7afa372382b3d671ab
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-01-12 16:12:59 +0100

    updated

--- app/todos/components/TodoView.tsx
+++ app/todos/components/TodoView.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
-import { View, StyleSheet, Alert } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Alert, StyleSheet, View } from 'react-native';
 import {
-    List, Colors, Portal, Dialog, Paragraph,
-    TextInput, Button, Switch, HelperText
+    List,
+    Colors,
+    Portal,
+    Dialog,
+    Paragraph,
+    TextInput,
+    Button,
+    Switch,
+    HelperText,
 } from 'react-native-paper';
 import { ITodoModel } from '../todo-model';
 import { deleteTodo, putTodo } from '../todo-service';
@@ -14,116 +21,144 @@ interface IProps {
 }
 
 const TodoView: React.FC<IProps> = ({ item, removeTodoFromList, updateList }) => {
+    const [visible, setVisible] = useState<boolean>(false);
+    const [todoForUpdate, setTodoForUpdate] = useState<ITodoModel>(
+        {} as ITodoModel,
+    );
+    const [updateLoading, setUpdateLoading] = useState<boolean>(false);
+    const [deleteLoading, setDeleteLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string>('');
 
-    const [visible, setVisible] = React.useState<boolean>(false);
-    const [deleteLoading, setDeleteLoading] = React.useState<boolean>(false)
-    const [error, setError] = React.useState<string>('')
-    const [todoForUpdate,
-        setTodoForUpdate] = React.useState<ITodoModel>(
-            {} as ITodoModel
-        );
-    const [updateLoading, setUpdateLoading] = React.useState<boolean>(false);
+    useEffect(() => {
+        setTodoForUpdate(item);
+    }, [item]);
 
     const deleteTodoFromDialog = async () => {
         setDeleteLoading(true);
         try {
-            await deleteTodo(item.id); // removing from the DB
-            removeTodoFromList(item.id) // removing from the UI
+            await deleteTodo(item.id);
+            removeTodoFromList(item.id);
+            setVisible(false);
         } catch (e) {
-            setError(e.message)
+            setError(e.message);
         }
         setDeleteLoading(false);
-    }
+    };
 
     const handleTitleChange = (input: string) => {
         let todo: ITodoModel = { ...todoForUpdate };
         todo.title = input;
         setTodoForUpdate(todo);
-    }
+    };
 
     const handleDescriptionChange = (input: string) => {
         let todo: ITodoModel = { ...todoForUpdate };
         todo.description = input;
         setTodoForUpdate(todo);
-    }
+    };
 
     const handleFinishedChange = () => {
-        setTodoForUpdate({
-            ...todoForUpdate,
-            finished: !todoForUpdate.finished,
-        })
-    }
+        setTodoForUpdate({ ...todoForUpdate, finished: !todoForUpdate.finished });
+    };
 
     const updateTodoFromDialog = async () => {
-        if (todoForUpdate.title.length === 0 ||
+        if (
+            todoForUpdate.title.length === 0 ||
             todoForUpdate.description.length === 0
         ) {
-            setError('Title and description are required');
+            setError('Title and description are required.');
             return;
         }
         setUpdateLoading(true);
         try {
             await putTodo(todoForUpdate);
             updateList(todoForUpdate);
-            setVisible(false)
+            setVisible(false);
         } catch (e) {
-            setError(error)
+            setError(error);
         }
         setUpdateLoading(false);
-    }
+    };
 
     return (
         <>
-            <List.Item onPress={() => setVisible(true)}
-                title={item.title} description={item.description}
+            <List.Item
+                onPress={() => {
+                    setVisible(true);
+                }}
+                title={item.title}
+                description={item.description}
                 right={otherProps => {
                     if (item.finished) {
                         return (
-                            <List.Icon {...otherProps}
+                            <List.Icon
+                                {...otherProps}
                                 color={Colors.green300}
-                                icon="check-circle" />
-                        )
+                                icon="check-circle"
+                            />
+                        );
                     }
+                    return null;
                 }}
             />
+
             <Portal>
-                <Dialog visible={visible}
-                    onDismiss={() => setVisible(false)}>
+                <Dialog visible={visible} onDismiss={() => setVisible(false)}>
                     <Dialog.Title>Edit your todo</Dialog.Title>
                     <Dialog.Content>
                         <View style={{ marginBottom: 20 }}>
                             <View style={styles.divider} />
-                            <TextInput value={todoForUpdate.title}
-                                onChangeText={handleTitleChange} />
+                            <TextInput
+                                value={todoForUpdate.title}
+                                onChangeText={handleTitleChange}
+                            />
                             <View style={styles.divider} />
-                            <TextInput value={todoForUpdate.description}
-                                multiline numberOfLines={4}
-                                onChangeText={handleDescriptionChange} />
+                            <TextInput
+                                value={todoForUpdate.description}
+                                multiline
+                                numberOfLines={4}
+                                onChangeText={handleDescriptionChange}
+                            />
                         </View>
-                        <View style={{ flexDirection: 'row', alignContent: 'center' }}>
-                            <Switch value={todoForUpdate.finished}
-                                onValueChange={handleFinishedChange} />
+                        <View
+                            style={{
+                                flexDirection: 'row',
+                                alignContent: 'center',
+                            }}>
+                            <Switch
+                                value={todoForUpdate.finished}
+                                onValueChange={handleFinishedChange}
+                            />
                             <Paragraph style={{ paddingLeft: 16, alignSelf: 'center' }}>
                                 Finished
-                            </Paragraph>
+              </Paragraph>
                         </View>
                         <HelperText type="error">{error}</HelperText>
                     </Dialog.Content>
                     <Dialog.Actions>
-                        <Button loading={deleteLoading}
-                            onPress={() => deleteTodoFromDialog()} >
+                        <Button
+                            loading={deleteLoading}
+                            onPress={() => deleteTodoFromDialog()}>
                             delete
-                        </Button>
+            </Button>
                         <View style={{ flex: 1 }} />
-                        <Button onPress={() => setVisible(false)} >Cancel</Button>
-                        <Button loading={updateLoading}
-                            onPress={() => updateTodoFromDialog()} >Update</Button>
+                        <Button
+                            onPress={() => {
+                                setVisible(false);
+                            }}>
+                            Cancel
+            </Button>
+                        <Button
+                            loading={updateLoading}
+                            onPress={() => updateTodoFromDialog()}>
+                            Update
+            </Button>
                     </Dialog.Actions>
                 </Dialog>
             </Portal>
         </>
-    )
-}
+    );
+};
 export default TodoView;
 
 const styles = StyleSheet.create({
