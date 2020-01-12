2c9a16290cc489a64b64c6c553d955dacb658396
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-01-12 12:22:33 +0100

    updated

--- app/navigation/root-navigation.tsx
+++ app/navigation/root-navigation.tsx
@@ -2,14 +2,21 @@ import * as React from 'react';
 import { NavigationNativeContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../auth/LoginScreen';
+import TodoListScreen from '../navigation/root-navigation'
 
 const Stack = createStackNavigator();
 
 export default function RootNavigation() {
     return (
         <NavigationNativeContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={LoginScreen} />
+            <Stack.Navigator initialRouteName="loginScreen">
+                <Stack.Screen
+                    name="loginScreen"
+                    component={LoginScreen} />
+                <Stack.Screen
+                    name="homeScreen"
+                    component={TodoListScreen}
+                    options={{ title: 'My Todo List' }} />
             </Stack.Navigator>
         </NavigationNativeContainer>
     );
