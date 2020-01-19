e87028412ac47ffaba02a46542286647a57ab6a7
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-01-19 02:00:43 +0100

    added type safety in services

--- app/todos/todo-service.ts
+++ app/todos/todo-service.ts
@@ -2,18 +2,18 @@ import { ITodoModel } from './todo-model';
 import { api } from '../api-config';
 
 export async function getTodos() {
-    return await api.get('todos');
+    return await api.get<ITodoModel[]>('todos');
 }
 
 export async function deleteTodo(id: string) {
-    return await api.delete('todos/' + id);
+    return await api.delete<void>('todos/' + id);
 }
 
 export async function postTodo(newTodo: ITodoModel) {
-    return await api.post('todos', newTodo);
+    return await api.post<ITodoModel>('todos', newTodo);
 }
 
 export async function putTodo(updatedTodo: ITodoModel) {
-    return await api.put(`todos/${updatedTodo.id}`, updatedTodo);
+    return await api.put<void>(`todos/${updatedTodo.id}`, updatedTodo);
 }
 
